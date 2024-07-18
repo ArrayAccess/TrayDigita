f304610a88889fa9f2731889afe5d147adcaccf9
ArrayIterator <37867551+ArrayIterator@users.noreply.github.com>
2024-07-19 01:53:47 +0700

    Update CapabilityInterface.php

--- src/Auth/Roles/Interfaces/CapabilityInterface.php
+++ src/Auth/Roles/Interfaces/CapabilityInterface.php
@@ -5,13 +5,45 @@ namespace ArrayAccess\TrayDigita\Auth\Roles\Interfaces;
 
 interface CapabilityInterface
 {
+    /**
+     * Check if the capability has a role
+     *
+     * @param RoleInterface|string $role
+     * @return bool
+     */
     public function has(RoleInterface|string $role) : bool;
 
+    /**
+     * Add a role to the capability
+     * @param RoleInterface|string $role
+     */
+    public function add(RoleInterface|string $role);
+
+    /**
+     * Get role identity
+     *
+     * @return string
+     */
     public function getIdentity(): string;
 
+    /**
+     * Get capability name
+     *
+     * @return string
+     */
     public function getName(): string;
 
+    /**
+     * Get capability description
+     *
+     * @return string|null
+     */
     public function getDescription(): ?string;
 
+    /**
+     * Get list of roles
+     *
+     * @return iterable<string, RoleInterface>
+     */
     public function getRoles() : iterable;
 }
